9d2d1e57bdf13ba3af21a7f466f9044f2000a7a3
frostuzi1 <valediomari@gmail.com>
2025-01-18 16:10:37 +0800

    add python files

--- python/index.py
+++ python/index.py
@@ -1,1 +1,18 @@
+#basic print statement
 print ("Hello World")
+
+#variables
+first_name = "Diomari Jose Vale"
+age = "22"
+print ("My name is " + first_name)
+print ("I am " + age + " years old")
+
+
+#function
+def my_function(fname):
+  print(" Intern " + fname )
+
+my_function("Jefferson Canamo")
+my_function("Kenneth Alvarado")
+my_function("Kylah Ostia")
+my_function("Diomari Vale")
